efc893d8b1db01064cdd5012c28c247bb7e6fc24
Ed Me <603803799@qq.com>
2018-04-25 15:56:13 +0800

    ...

--- gitPull.py
+++ gitPull.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-print sys.argv
+    print sys.argv
     os.chdir(sys.argv[1])
     os.system('git pull origin master')
     os.system('pm2 restart main')
