f48b1bffb819221fe0986cae782572b83dd2ab44
Ed Me <603803799@qq.com>
2018-04-25 11:20:26 +0800

    ...

--- main.js
+++ main.js
@@ -38,26 +38,25 @@ app.get('/', function (req, res) {
   /*res.render('index', {title: 'TEST', message: 'Hello Visitor!'})*/
 })
 
-app.get('/:path', function (req, res) {
-  if (req.params.path === 'pushCode') { // github push or commit
-    process.exec('python gitPull.py', (error, stdout, stderr) => {
-      if (error) return res.send('error:Command failed')
-      else return res.sendStatus(200)
-    })
-  }
-  else { // user get file
-    const filePath = path.join(__dirname, 'components', `${req.params.path}.md`)
-    fs.stat(filePath, function (error, stats) {
-      if (error) return res.send('错误的路径')
-      if (stats.isFile()) {
-        return res.render('index', {
-          title: req.params.path,
-          html: marked(fs.readFileSync(filePath, 'utf-8')),
-        })
-      }
-      return res.send('not path')
-    })
-  }
+app.get('/:path', function (req, res) {  // user get file
+  const filePath = path.join(__dirname, 'components', `${req.params.path}.md`)
+  fs.stat(filePath, function (error, stats) {
+    if (error) return res.send('错误的路径')
+    if (stats.isFile()) {
+      return res.render('index', {
+        title: req.params.path,
+        html: marked(fs.readFileSync(filePath, 'utf-8')),
+      })
+    }
+    return res.send('not path')
+  })
+})
+
+app.post('/pushCode', function (req, res) { // github push or commit
+  process.exec('python gitPull.py', (error, stdout, stderr) => {
+    if (error) return res.send('error:Command failed')
+    else return res.sendStatus(200)
+  })
 })
 
 marked.setOptions({
